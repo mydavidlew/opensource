23248a1c8232f058d0de3e71f02bb16b45f00546
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-09-05 22:26:04 +0800

    Add files via upload

--- streamlit/pages/myapp05.py
+++ streamlit/pages/myapp05.py
@@ -2,20 +2,24 @@ import streamlit as st
 import pandas as pd
 import os
 
+from pathlib import Path
 from datasets import load_dataset
 from haystack import Pipeline, Document
 from haystack.utils import ComponentDevice
 from haystack.components.fetchers import LinkContentFetcher
 from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
-from haystack.components.converters import HTMLToDocument, TextFileToDocument
+from haystack.components.converters import HTMLToDocument, MarkdownToDocument, PyPDFToDocument, TextFileToDocument
 from haystack.components.embedders import SentenceTransformersTextEmbedder, SentenceTransformersDocumentEmbedder
 from haystack.components.readers import ExtractiveReader
+from haystack.components.routers import FileTypeRouter
+from haystack.components.joiners import DocumentJoiner
 from haystack.components.builders import PromptBuilder
 from haystack.components.preprocessors import DocumentSplitter, DocumentCleaner
 from haystack.components.generators import OpenAIGenerator, HuggingFaceLocalGenerator
 from haystack.components.writers import DocumentWriter
 from haystack.document_stores.types import DuplicatePolicy
 from haystack.document_stores.in_memory import InMemoryDocumentStore
+from pandas.io.json import to_json
 from tensorflow.python.ops.summary_ops_v2 import write
 from tensorflow.tools.docs.doc_controls import header
 
@@ -34,9 +38,19 @@ dlwritetoken_key = "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"
 if "HF_API_TOKEN" not in os.environ:
     os.environ["HF_API_TOKEN"] = dlreadtoken_key
 
+filenamex = "datasets/Malaysia_Corruption_1MDB.txt"
+filehandler = open(filenamex, "r")
+dataset = filehandler.read()
+filehandler.close()
 
-dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
-documents = [Document(content=doc["content"], meta=doc["meta"]) for doc in dataset]
+#dataset = load_dataset("text", data_files=filenamex, split="train")
+
+documents = [Document(content=dataset, meta={"name": filenamex})]
+
+#dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
+#documents = [Document(content=doc["content"], meta=doc["meta"]) for doc in dataset]
+
+st.write(dataset)
 
 template = """Given these documents, answer the question.
               Documents:
@@ -51,21 +65,42 @@ model = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
 
 document_store = InMemoryDocumentStore()
 indexing_pipeline = Pipeline()
-indexing_pipeline.add_component(name="converter", instance=TextFileToDocument(encoding="utf-8"))
-indexing_pipeline.add_component(name="cleaner", instance=DocumentCleaner())
-indexing_pipeline.add_component(name="splitter", instance=DocumentSplitter(split_by="word", split_length=200, split_overlap=10)))
-indexing_pipeline.add_component(name="embedder", instance=SentenceTransformersDocumentEmbedder(model=model))
+#indexing_pipeline.add_component(name="file_type_router", instance=FileTypeRouter(mime_types=["text/plain", "text/markdown", "application/pdf"]))
+#indexing_pipeline.add_component(name="ptext_converter", instance=TextFileToDocument(encoding="utf-8"))
+#indexing_pipeline.add_component(name="markdown_converter", instance=MarkdownToDocument())
+#indexing_pipeline.add_component(name="pdf_converter", instance=PyPDFToDocument())
+#indexing_pipeline.add_component(name="joiner", instance=DocumentJoiner())
+#indexing_pipeline.add_component(name="cleaner", instance=DocumentCleaner())
+indexing_pipeline.add_component(name="splitter", instance=DocumentSplitter(split_by="word", split_length=200, split_overlap=50))
+indexing_pipeline.add_component(name="embedder", instance=SentenceTransformersDocumentEmbedder(model=model, progress_bar=True))
 indexing_pipeline.add_component(name="writer", instance=DocumentWriter(document_store=document_store))
-indexing_pipeline.connect("converter.documents", "cleaner.documents")
-indexing_pipeline.connect("cleaner.documents", "splitter.documents")
+#indexing_pipeline.connect("file_type_router.text/plain", "ptext_converter.sources")
+#indexing_pipeline.connect("file_type_router.text/markdown", "markdown_converter.sources")
+#indexing_pipeline.connect("file_type_router.application/pdf", "pdf_converter.sources")
+#indexing_pipeline.connect("ptext_converter.documents", "joiner.documents")
+#indexing_pipeline.connect("markdown_converter.documents", "joiner.documents")
+#indexing_pipeline.connect("pdf_converter.documents", "joiner.documents")
+#indexing_pipeline.connect("joiner.documents", "cleaner.documents")
+#indexing_pipeline.connect("cleaner.documents", "splitter.documents")
 indexing_pipeline.connect("splitter.documents", "embedder.documents")
 indexing_pipeline.connect("embedder.documents", "writer.documents")
-indexing_pipeline.run(data={"converter": {"documents": documents}})
+
+#indexing_pipeline.run({"file_type_router": {"sources": list(Path(output_dir).glob("**/*"))}})
+#indexing_pipeline.run({"ptext_converter": {"sources": filenamex}})
+
+indexing_pipeline.run(data={"splitter": {"documents": documents}})
 
 
 reader_answer = ExtractiveReader(no_answer=False)
 reader_answer.warm_up()
-generator = OpenAIGenerator()
+#generator = OpenAIGenerator()
+generator = HuggingFaceLocalGenerator(
+        model="HuggingFaceTB/SmolLM-1.7B-Instruct",
+        huggingface_pipeline_kwargs={"device_map": "auto",
+                                     "model_kwargs": {}},
+        generation_kwargs={"max_new_tokens": 500})
+# Start the Generator
+generator.warm_up()
 
 retriever_store = InMemoryEmbeddingRetriever(document_store=document_store, top_k=5)
 querying_pipeline = Pipeline()
@@ -73,16 +108,18 @@ querying_pipeline.add_component(name="embedder", instance=SentenceTransformersTe
 querying_pipeline.add_component(name="retriever", instance=retriever_store)
 querying_pipeline.add_component(name="reader", instance=reader_answer)
 querying_pipeline.add_component(name="prompt_builder", instance=prompt_builder)
-querying_pipeline.add_component(name="llm", instance=generator)
+querying_pipeline.add_component(name="generator", instance=generator)
 querying_pipeline.connect("embedder.embedding", "retriever.query_embedding")
 querying_pipeline.connect("retriever.documents", "reader.documents")
 querying_pipeline.connect("retriever.documents", "prompt_builder.documents")
-querying_pipeline.connect("prompt_builder", "llm")
+querying_pipeline.connect("prompt_builder", "generator")
 
 
-query = "Who was Pliny the Elder?"
-answer = querying_pipeline.run(
-    data={"embedder": {"text": query}, "retriever": {"top_k": 3}, "reader": {"query": query, "top_k": 2}}
-)
-
-st.write(answer)
+query = "What si 1MDB case?"
+answer = querying_pipeline.run(data={"embedder": {"text": query},
+                                     "retriever": {"top_k": 3},
+                                     "reader": {"query": query, "top_k": 2},
+                                     "generator": {"generation_kwargs": {"max_new_tokens": 350}}})
+st.write("a->", answer)
+st.write("b->", answer["answers"][0].score, answer["answers"][0].content)
+st.write("c->", reader_answer["answers"][0].data)
