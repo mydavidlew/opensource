d7fc8f080ae024a51ddb45e84ad07e51951262fd
davidlew <12636609+mydavidlew@users.noreply.github.com>
2024-09-22 00:55:33 +0800

    Add files via upload

---
+++ pyGenAICode/helper/config.py
@@ -0,0 +1,8 @@
+# OpenAI
+openai_key = "sk-pr"+"oj-tOlDkDvCjXIfDZeoOJJJT3BlbkFJdSyAfPKCBrK7u9d7wlM8"
+# HuggingFace
+dlreadtoken_key = "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"
+dlwritetoken_key = "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"
+# OpenAI
+DefaultProject_key = "sk-pr"+"oj-LaYTCPr8Jvqyx_w8tZ5jtZ56-7j4ks2Pfwgb578rFs4T-lfbw-Muv-fDhNT3BlbkFJ8M3piIy-sbaU-_3323nqght9wC2xtfBupXoGiRCTthGYkf6gkg_r8hvNIA"
+Application_key = "sk-BmZ"+"m896KKtezM6HTQV-jsFXQPqJqsLz-mbOYbuoBe0T3BlbkFJAc-ErVPAJ-UdYjTGWiXctGuFCHa46nrHcSY_sWo4UA"
---
+++ pyGenAICode/run.sh
@@ -0,0 +1,10 @@
+streamlit run AI_Platforms.py --server.port=8501 --server.address 0.0.0.0 --server.baseUrlPath=/ai
+
+# Create an environment using venv
+#> python -m venv .venv
+# Activate your environment - macOS & Linux
+#> source .venv/bin/activate
+# Test that the installation worked
+#> streamlit hello
+# To exit and return to your normal shell
+#> deactivate
