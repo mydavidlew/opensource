34ec2ccbef673694bc62e973b1aceb43b9c98efd
davidlew <12636609+mydavidlew@users.noreply.github.com>
2025-03-17 19:21:01 +0800

    Update 5Computer_Vision.py

--- genaicode/pages/5Computer_Vision.py
+++ genaicode/pages/5Computer_Vision.py
@@ -524,8 +524,8 @@ def DETR_Visualize4():
 
     for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
         box = [round(i, 2) for i in box.tolist()]
-        print(f"Detected {model.config.id2label[label.item()]} with confidence "
-              f"{round(score.item(), 3)} at location {box}")
+        st.write(f"Detected :blue[**{model.config.id2label[label.item()]}**] with confidence "
+                 f":blue[**{round(score.item(), 3)}**] at location :green[**{box}**]")
 
 def ViT_01():
     # ViT Object Detection (using a library like timm)
